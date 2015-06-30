2862823dfd1151e854325dad077554fcbce99fa1
Eric Yu <echenyu@umich.edu>
2015-06-30 08:06:07 -0400

    First
    First Commit

---
+++ GEMobileApp/GEMobileApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GEMobileApp
+//
+//  Created by Eric Yu on 6/30/15.
+//  Copyright (c) 2015 EricYu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GEMobileApp/GEMobileApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GEMobileApp
+//
+//  Created by Eric Yu on 6/30/15.
+//  Copyright (c) 2015 EricYu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
