a72f68e6b904e2538448291d0f3560fe8bffa8e9
Eric Yu <echenyu@umich.edu>
2015-07-07 16:03:07 -0400

    added the login view controller. Made story board changes here to prevent merge conflicts

---
+++ GEMobileApp/GEMobileApp/MapTableViewController.h
@@ -0,0 +1,15 @@
+//
+//  MapTableViewController.h
+//  
+//
+//  Created by Eric Yu on 7/7/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MapTableViewController : UITableViewController
+
+@property (nonatomic, strong) NSMutableArray *items;
+
+@end
